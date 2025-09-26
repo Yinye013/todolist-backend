8300ca27db7d71fc9b36ebc8a9a1dc69042ae076
onyinyechukwu <yinadesanya@gmail.comgit config --global user.name Yinye013>
2025-09-26 01:03:53 +0100

    remove unnecessary blank line in UserRepository.java

--- src/main/java/com/todo/todolist/repositories/UserRepository.java
+++ src/main/java/com/todo/todolist/repositories/UserRepository.java
@@ -8,5 +8,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
-
 }
